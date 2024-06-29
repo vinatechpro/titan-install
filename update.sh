2571776324e6795968e69d80367ff5db48338133
vinatechpro <149946061+vinatechpro@users.noreply.github.com>
2024-06-29 15:33:31 +0700

    Update update.sh - titan-l2edge_v0.1.19_patch_linux_amd64
    The CLI program of L2 node has a Bug. Procedure Will result in no reward other than the storage task.
    Please update to the latest version
    Linux：
    https://github.com/Titannet-dao/titan-node/releases/download/v0.1.19/titan-l2edge_v0.1.19_patch_linux_amd64.tar.gz
    
    Mac
    https://github.com/Titannet-dao/titan-node/releases/download/v0.1.19/titan-l2edge_v0.1.19_patch_darwin_amd64.tar.gz
    
    
    Windows：
    https://github.com/Titannet-dao/titan-node/releases/download/v0.1.19/titan-l2edge_v0.1.19_patch_windows_amd64.tar.gz

--- update.sh
+++ update.sh
@@ -1,24 +1,40 @@
 #!/bin/bash
 VERSION='v0.1.19'
-echo "Update Titan node $VERSION"
+echo "Update Titan node ${VERSION}"
 echo "..."
 
 echo "Stop titand.service & remove old version"
 systemctl stop titand.service && rm -rf /usr/local/titan
 echo "..."
 
-echo "Download Titan node $VERSION"
+echo "Download Titan node ${VERSION}"
 # Tải xuống tệp cài đặt Titan
-wget https://github.com/Titannet-dao/titan-node/releases/download/$VERSION/titan-edge_${VERSION}_linux_amd64.tar.gz
+wget https://github.com/Titannet-dao/titan-node/releases/download/${VERSION}/titan-l2edge_${VERSION}_patch_linux_amd64.tar.gz
 
-# Giải nén tệp cài đặt
-sudo tar -xf titan-edge_${VERSION}_linux_amd64.tar.gz -C /usr/local
+sudo tar -xf titan-l2edge_${VERSION}_patch_linux_amd64.tar.gz -C /usr/local
 
-# Di chuyển thư mục giải nén đến vị trí cài đặt
-sudo mv /usr/local/titan-edge_${VERSION}_linux_amd64 /usr/local/titan
+sudo mv /usr/local/titan-l2edge_${VERSION}_patch_linux_amd64 /usr/local/titan
+sudo cp /usr/local/titan/libgoworkerd.so /usr/lib/libgoworkerd.so
 
-# Xóa tệp cài đặt đã giải nén
-rm titan-edge_${VERSION}_linux_amd64.tar.gz
+rm titan-l2edge_${VERSION}_patch_linux_amd64.tar.gz
+
+# Định nghĩa nội dung cần thêm
+content="
+export PATH=\$PATH:/usr/local/titan
+export LD_LIBRARY_PATH=\$LD_LIZBRARY_PATH:./libgoworkerd.so
+"
+
+# Kiểm tra nếu file ~/.bash_profile chưa tồn tại thì tạo mới, nếu đã tồn tại thì ghi thêm
+if [ ! -f ~/.bash_profile ]; then
+  echo "$content" > ~/.bash_profile
+  source ~/.bash_profile
+else
+  echo "$content" >> ~/.bash_profile
+  source ~/.bash_profile
+fi
+
+echo "Export PATH ~/.bash_profile"
+
 
 echo "Restart titand.service"
 sudo systemctl restart titand.service
